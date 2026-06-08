8d04381ad4c65dda6d65d475bd26c6d048349b92
Alister Scott <alister.scott@gmail.com>
2026-06-08 13:36:08 +1000

    Added a test for 1 day ago

--- tests/countdownup.spec.ts
+++ tests/countdownup.spec.ts
@@ -257,3 +257,67 @@ test("can see all display sizes for a date 2 days ago", async ({ page }) => {
     fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
   }
 });
+
+test("can see all display sizes for a date 1 day ago", async ({ page }) => {
+  const sourceFile = path.join(__dirname, "1dayago.trmnlp.yml");
+  const destFile = path.join(__dirname, "..", ".trmnlp.yml");
+  const expectedHeadingText = "it has been";
+  const expectedDaysText = "1";
+  const expectedText = "day since";
+  const expectedDateDay = "7";
+  const expectedDateMonth = "June";
+  const expectedDateYear = "2026";
+  const expectedFullDate = `${expectedDateDay} ${expectedDateMonth} ${expectedDateYear}`;
+
+  if (!fs.existsSync(sourceFile)) {
+    throw new Error(`Source file not found: ${sourceFile}`);
+  }
+
+  if (!fs.existsSync(destFile)) {
+    throw new Error(`Destination file not found: ${destFile}`);
+  }
+
+  const originalContent = fs.readFileSync(destFile, "utf-8"); // Save original content to restore later
+
+  fs.copyFileSync(sourceFile, destFile);
+
+  try {
+    const routes = ["/full", "/half_horizontal", "/half_vertical", "/quadrant"];
+
+    for (const route of routes) {
+      await test.step(`Testing route: ${route}`, async () => {
+        await page.goto(route);
+        await page.getByRole("link", { name: "Poll" }).click();
+        const trmnlFrame = page.frameLocator("iframe");
+        if (route === "/full" || route === "/quadrant") {
+          await expect
+            .soft(trmnlFrame.locator("div.fdp-heading"))
+            .toHaveText(expectedHeadingText);
+        }
+        await expect
+          .soft(trmnlFrame.locator("div.fdp-count"))
+          .toHaveText(expectedDaysText);
+        await expect
+          .soft(trmnlFrame.locator("div.fdp-relation"))
+          .toHaveText(expectedText);
+        if (route !== "/half_vertical") {
+          await expect
+            .soft(trmnlFrame.locator("div.fdp-target-day"))
+            .toHaveText(expectedDateDay);
+          await expect
+            .soft(trmnlFrame.locator("div.fdp-target-month"))
+            .toHaveText(expectedDateMonth);
+          await expect
+            .soft(trmnlFrame.locator("div.fdp-target-year"))
+            .toHaveText(expectedDateYear);
+        } else {
+          await expect
+            .soft(trmnlFrame.locator("div.fdp-target-combined"))
+            .toHaveText(expectedFullDate);
+        }
+      });
+    }
+  } finally {
+    fs.writeFileSync(destFile, originalContent, "utf-8"); // Restore original content
+  }
+});
