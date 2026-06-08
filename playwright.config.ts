ef7312b0e960248749936de4d15458da1ff4bd87
Alister Scott <alister.scott@gmail.com>
2026-06-08 14:39:45 +1000

    retry on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   testDir: "./tests",
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
 
   workers: 1,
   reporter: "html",
